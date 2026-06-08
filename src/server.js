ea62b1974a6ee17f336e75e17dee29608ef896f1
Jose <netobelem22@gmial.com>
2026-06-08 02:53:38 -0300

    debug: log env vars and simplify PrismaClient init

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 require('dotenv').config()
+console.log('DATABASE_URL set:', !!process.env.DATABASE_URL)
+console.log('NODE_ENV:', process.env.NODE_ENV)
 const app = require('./app')
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
