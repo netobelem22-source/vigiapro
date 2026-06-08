e9d7680820cd57615c37ac1350a1eba19c246814
Jose <netobelem22@gmial.com>
2026-06-08 02:48:34 -0300

    fix: passa DATABASE_URL explicitamente ao PrismaClient

--- src/utils/prisma.js
+++ src/utils/prisma.js
@@ -1,6 +1,8 @@
 const { PrismaClient } = require('@prisma/client')
 
-const prisma = global.prisma || new PrismaClient()
+const prisma = global.prisma || new PrismaClient({
+  datasources: { db: { url: process.env.DATABASE_URL } }
+})
 
 if (process.env.NODE_ENV !== 'production') {
   global.prisma = prisma
